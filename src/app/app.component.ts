c6d8f994fad2eff44ef313f4819f6b99a8d559ab
Eduardo Nuñez <eduardodnf@gmail.com>
2024-09-29 14:19:03 -0300

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./componentes/header/header.component";
+import { HomeComponent } from "./componentes/home/home.component";
+import { CategoriasComponent } from "./componentes/categorias/categorias.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
+import { CarruselComponent } from "./componentes/carrusel/carrusel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, CategoriasComponent, FooterComponent, CarruselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'publicidad-negocios';
+  cityName? = 'Concordia';
+
 }
